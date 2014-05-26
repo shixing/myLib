05765e4c58db485fe5ac112302a64b87594f7598
Shi Xing <shixing19910105@gmail.com>
2014-05-26 11:53:53 -0700

    add config and pinyin

--- README.md
+++ README.md
@@ -10,5 +10,9 @@ the code repository for future reuse
 #### /config
 A python module read and write config files
 
+#### /pinyin
+A python module that can convert chinese characters to pinyin 
+Copyright (c) 2004 onward  Yusuke Shinyama <yusuke at cs dot nyu dot edu>
+
 ### bin
 A frequently used command line tools, including text preprocessing, multiprocessing, personal task load balance and other tools.
--- python/config/config.py
+++ python/config/config.py
@@ -1,61 +1,20 @@
-# Supported type : float,int,bool,string,numpy.float64
-# Supported feature : '#' comments
-#
-
-import numpy 
-
-def get_config(fn):
-    config = {}
-    f = open(fn)
-    for line in f:
-        if line.strip().startswith('#'):
-            continue
-        fields = line.strip().split('=')
-        if len(fields)<3:
-            continue
-
-        t = fields[0]
-        key = fields[1]
-        value = fields[2]
-        if t == 'i': #int
-            config[key] = int(value)
-        elif t== 'f': #float
-            config[key] = float(value)
-        elif t=='s': # string
-            config[key] = value
-        elif t=='b': # bool
-            if value == 'True':
-                config[key]= True
-            elif value == 'False':
-                config[key]=False
-    return config
-
-
-def write_config(config,fn):
-    f = open(fn,'w')
-    temp  = []
-    for key in config:
-        value = config[key]
-        type_str = ''
-        if type(value) == type(' '):
-            type_str = 's'
-        elif type(value) == type(1):
-            type_str = 'i'
-        elif type(value) == float or type(value) == numpy.float64:
-            type_str = 'f'
-        elif type(value) == type(True):
-            type_str = 'b'
-        value = str(value)
-        temp.append([type_str,key,value])
-
-    temp = ['='.join(x) for x in temp]
-    temp = sorted(temp)
-    temp_str = '\n'.join(temp)
-    f.write(temp_str)
-    f.close()
+import configparser
 
 def demo():
-    path = './train.config'
-    config = get_config(path)
-    write_config(config,path)
-
+    config_fn = './train.cfg'
+    config = configparser.ConfigParser()
+    config._interpolation = configparser.ExtendedInterpolation()
+    config.read(config_fn)
+    
+    # get str
+    # get(session_name,field_name)
+    config.get('path','reference')
+    
+    # get int
+    config.getint('decoding','nthread')
+    
+    # set value
+    # set(session_name,field_name,value_str)
+    # value should always be strings
+    weight = 0.4
+    config.set('weights','wlm',str(weight))
